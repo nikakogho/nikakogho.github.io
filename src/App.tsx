c94cc11fe57c88714edad8dfcd9276a7e30f4050
nikakogho <nikakoghuashvili@gmail.com>
2025-04-14 19:35:27 +0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,8 @@ function App() {
   return (
     <Layout>
       <Routes>
-        {/* Main site routes */}
         <Route path="/" element={<HomePage />} />
 
-        {/* --- Updated Vault Root Route --- */}
         <Route path="/vaults/:vaultId" element={<VaultHomeRedirect />} />
         <Route path="/vaults/:vaultId/notes/*" element={<NotePage />} />
 
