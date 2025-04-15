9dd31e630213a0a8796033ca54eeb08c42514f3d
nikakogho <nikakoghuashvili@gmail.com>
2025-04-15 16:49:11 +0400

    react updated to also have AI vault

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const VAULT_IDS = ['Neuroscience', 'Space', 'Bioengineering', 'Robots'];
+const VAULT_IDS = ['Neuroscience', 'Space', 'Bioengineering', 'Robots', 'AI'];
 
 const Header: React.FC = () => {
   return (
